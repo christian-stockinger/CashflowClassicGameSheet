a6053e42741369ddae7af27ba03c0aab01eacfc2
Christian Stockinger <mail@christian-stockinger.de>
2023-10-04 23:35:07 +0200

    feat: implement Basic Dashboard UI

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card variant="tonal" width="100%" class="mx-auto ma-2">
+    <v-card-title class="text-h6 text-md-h5 text-lg-h4">{{ title }}</v-card-title>
+    <v-card-text>
+      {{ text }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'InfoCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,66 @@
 <template>
   <v-container fluid>
-    <v-responsive class="align-center text-center fill-height">
-      <h1 class="text-h3 font-weight-bold">Dashboard</h1>
-    </v-responsive>
+    <v-row>
+      <v-text-field label="Beruf" v-model="job"></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field label="Traum" v-model="dream"></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field label="Auditor" v-model="auditor"></v-text-field>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-col class="ma-2">
+        <v-row>
+          <InfoCard title="Monatlicher Cashflow" :text="state.GetCashflow + ' €'"/>
+        </v-row>
+        <v-row>
+          <InfoCard title="Kontostand" :text="state.balance + ' €'"/>
+        </v-row>
+      </v-col>
+
+      <v-col class="ma-2">
+        <v-row>
+          <InfoCard title="Gehalt" :text="state.balance + ' €'"/>
+        </v-row>
+        <v-row>
+          <InfoCard title="Passives Einkommen" :text="state.passivesEinkommen + ' €'"/>
+        </v-row>
+        <v-row>
+          <InfoCard title="Gesamte Ausgaben" :text="state.gesammteAusgaben + ' €'"/>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-btn
+        block
+        class="text-none"
+        color="grey-lighten-3"
+        size="x-large"
+        variant="flat"
+        @click="state.CalculatePayday()"
+      >
+        Zahltag
+      </v-btn>
+    </v-row>
+
   </v-container>
 </template>
 
-<script setup>
+<script>
+import InfoCard from "@/components/InfoCard.vue";
+
+export default {
+  components: {InfoCard},
+  data() {
+    return {
+      state: this.gamestate,
+      job: "",
+      dream: "",
+      auditor: "",
+    };
+  }
+};
 </script>
